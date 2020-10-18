85aea3708ef9ec7921b14c51389a76e8941803ef
Jamie Chang <36315888+pixelpad-io@users.noreply.github.com>
2020-10-18 16:57:48 -0700

    Committed: 1603065468

---
+++ 210992.php
@@ -0,0 +1,63 @@
+stdClass Object
+(
+    [assets] => stdClass Object
+        (
+            [code] => stdClass Object
+                (
+                    [main] => //hello world !
+
+
+//rochelle was here.
+
+
+
+//jamie was here
+
+
+
+
+this is a php file
+                )
+
+            [model] => stdClass Object
+                (
+                )
+
+            [texture] => stdClass Object
+                (
+                    [earth.png] => stdClass Object
+                        (
+                            [uri] => https://s3.us-west-1.amazonaws.com/test.pixelpad.io/__PIXELPAD_ASSET__.1.210992.earth.png
+                        )
+
+                )
+
+            [sound] => stdClass Object
+                (
+                )
+
+            [library] => stdClass Object
+                (
+                )
+
+            [scene] => stdClass Object
+                (
+                    [MainScene.scn] => {
+   "t":"Scene",
+   "rootObjs":[
+      
+   ]
+}
+                )
+
+            [material] => stdClass Object
+                (
+                )
+
+            [prefab] => stdClass Object
+                (
+                )
+
+        )
+
+)
